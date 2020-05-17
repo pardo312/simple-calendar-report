0c9bf889615713779870d6626d6102b960e7938c
pardo312 <pardod312@gmail.com>
2020-05-17 17:01:21 -0500

    🌐Heroku try🌐

---
+++ build/precache-manifest.d87d3e6741a378ef775c6a8dff2c0c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84db5d8fc2baf400370e7ae86860480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2399964b554aac023ba8",
+    "url": "/static/css/main.318392bd.chunk.css"
+  },
+  {
+    "revision": "5da894e2325fc53e01d1",
+    "url": "/static/js/2.0351cfe5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0351cfe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2399964b554aac023ba8",
+    "url": "/static/js/main.40c9862b.chunk.js"
+  },
+  {
+    "revision": "5d2d2efb4ffc056fc787",
+    "url": "/static/js/runtime-main.0bb68df3.js"
+  }
+]);
